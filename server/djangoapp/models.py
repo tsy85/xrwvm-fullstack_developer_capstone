a574f507c272d943ff1759b365b8a61df38f5ddf
tsy85 <vitor.lazarini@gmail.com>
2025-07-11 09:21:05 -0400

    Add dynamic pages 2

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,15 +1,14 @@
 # Uncomment the following imports before adding the Model code
-
 from django.db import models
-# from django.utils.timezone import now
+from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
 # Create your models here.
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
@@ -29,10 +28,8 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
-
 class CarModel(models.Model):
-    # Many-to-One relationship
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -41,9 +38,11 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(
-        default=2023,
-        validators=[MaxValueValidator(2023), MinValueValidator(2015)])
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
     # Other fields as needed
 
     def __str__(self):
